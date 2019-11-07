b4d2b29f2e3371c7af52363d387bcda86a34218a
Filip Olsén <filip.olsen@hotmail.com>
2019-11-07 15:25:12 +0100

    Change from dvorak to qwerty.

--- keyboards/jj40/keymaps/sudocoffee-svorak/keymap.c
+++ keyboards/jj40/keymaps/sudocoffee-svorak/keymap.c
@@ -32,12 +32,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = LAYOUT_ortho_4x12( \
-  KC_TAB,    NO_AA,   KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,   NO_APOS, \
-  KC_LSFT,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,   KC_SLSH, \
-  KC_ESC,    NO_OSLH, NO_AE,   KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,   KC_Z, \
+  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,      NO_AA, \
+  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     NO_OSLH,   NO_AE, \
+  KC_ESC,    NO_GRTR, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,    KC_SLSH, \
   MO(_FUNC), KC_LGUI, KC_LALT, KC_LCTL, KC_SPC,  MO(_LOWER),  MO(_RAISE),  KC_BSPC,   KC_ENT, KC_HOME, KC_END,   KC_LALT \
 ),
-
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
